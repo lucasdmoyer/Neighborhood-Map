d0281d3b4336efb890becf62224feb425c4b5faa
Lucas Moyer <Lucas Moyer>
2017-01-18 22:45:35 -0800

    type: feat.
    body: Added focus on Newport Oregon and identifed some important locations
    
    footer: Version 5

--- js/app.js
+++ js/app.js
@@ -1,86 +1,27 @@
-var initialCats =  [
-        {
-            clickCount : 0,
-            name : 'Tabby',
-            imgSrc : 'images/cat1.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
-        	nicknames: ['asdf', 'asdfdsa']
-        },
-        {
-            clickCount : 0,
-            name : 'Tiger',
-            imgSrc : 'images/cat1.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
-        	nicknames: ['asdf', 'asdfdsa', 'asdfas']
-        },
-        {
-            clickCount : 0,
-            name : 'Scaredy',
-            imgSrc : 'images/cat1.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
-        },
-        {
-            clickCount : 0,
-            name : 'Shadow',
-            imgSrc : 'images/cat1.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
-        },
-        {
-            clickCount : 0,
-            name : 'Sleepy',
-            imgSrc : 'images/cat1.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
-        }
-    ]
-
-// cat list
-// make cats clickable
-
-var ViewModel = function() {
-	var self = this;
-	this.catList = ko.observableArray([]);
-
-	initialCats.forEach(function(catItem){
-		self.catList.push(new Cat(catItem));
-	});
-
-
-	this.currentCat = ko.observable(this.catList()[0]);
-	// with the with: currentCat binding context, this is a cat
-	this.incrementcounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
-
-	this.changeCat = function(cat) {
-		self.currentCat(cat);
-	};
-
-}
-/*
-var List = function() {
-	this.newList = ko.observableArray(ViewModel.catList);
-	console.log(this.newList);
-	return this.newList;
-} */
-
-var Cat = function(data) {
-	this.clickCount = ko.observable(data.clickCount);
-	// connects to data-bind="text : name"
-	this.name = ko.observable(data.name);
-	this.imgSrc = ko.observable(data.imgSrc);
-	this.imgAttribution = ko.observable(data.imgAttribution);
-	this.nicknames = ko.observableArray(data.nicknames);
-
-	this.level = ko.computed(function() {
-		if (this.clickCount() < 5) {
-			return 'infant';
-		} else {
-			return 'adult';
-		}
-	}, this);
-
-
-
+// Class to represent a row in the seat reservations grid
+function location(name, lat, long) {
+    var self = this;
+    self.name = name;
+    self.lat = lat;
+    self.long = long;    
 }
 
-ko.applyBindings(new ViewModel());
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+    var self = this;   
+
+    // Editable data
+    self.locations = ko.observableArray([
+        new location("Starbucks", 44.63731107, -124.05294478),
+        new location("JC Thriftway", 44.63726527, -124.05372798)
+    ]);
+ 
+
+    // Operations
+    self.addLocation = function() {
+        self.locations.push(new Location("", 44, -124));
+    }
+    self.removeSeat = function(location) { self.locations.remove(location) }
+}
+
+ko.applyBindings(new ReservationsViewModel());
