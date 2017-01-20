d14da5d2e4bcbfd69af3ba252f80a9980393b346
Lucas Moyer <Lucas Moyer>
2017-01-19 20:27:53 -0800

    type: feat.
    body: Added a filter function
    
    footer: Version 8

--- js/app.js
+++ js/app.js
@@ -1,55 +1,3 @@
-function initMap() {
-
-	var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 15,
-			center: {lat: 44.636, lng: -124.053}
-		});
-
-    // Create an array of alphabetical characters used to label the markers.
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
-    
-    /*
-    var markers = locations.map(function(location, i) {
-      return new google.maps.Marker({
-        position: location,
-        label: locations[i].name
-      });
-    });
-*/
-	/*
-	for (i=0; i<ViewModel().length; i++) {
-		console.log(ViewModel()[i].name);
-		newMarker = ViewModel()[i];
-		var marker = new google.maps.Marker({
-			lat: newMarker.lat,
-			lng: newMarker.lng,
-			map: map
-		});
-	} */
-
-	for (i=0; i<ViewModel().length; i++) {
-		var data = ViewModel()[i];
-		var latLng = new google.maps.LatLng(data.lat, data.lng);
-		console.log(data.lng);
-
-		var marker = new google.maps.Marker({
-			position: {lat: data.lat, lng: data.lng},
-			map: map,
-			title: data.name
-		});
-	}
-
-    // Add a marker clusterer to manage the markers.
-    //var markerCluster = new MarkerClusterer(map, markers,
-        //{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-}
-
-
 var locations =  [
         {
             name : 'Starbucks',
@@ -63,33 +11,45 @@ var locations =  [
         }
     ]
 
-
 // Class to represent a row in the seat reservations grid
 function Location(name, lat, lng) {
     var self = this;
-    self.name = name;
-    self.lat = lat;
-    self.lng = lng;  
+    this.name = ko.observable(name);  
+    this.lat = ko.observable(lat);
+    this.lng = ko.observable(lng);
 }
-
 // Overall viewmodel for this screen, along with initial state
 function ViewModel() {
     var self = this;
- 	
-    self.locationList = ko.observableArray([]);
+    //self.locationList = ko.observableArray(locations);
+    self.filter = ko.observable("");
+	self.locationList = ko.observableArray([
+	    { name: "Bungle", type: "Bear" },
+	    { name: "George", type: "Hippo" },
+	    { name: "Zippy", type: "Unknown" }
+	]);
 
-    locations.forEach(function(locationItem){
-    	self.locationList.push(new Location(name=locationItem.name,lat=locationItem.lat, lng=locationItem.lng));
-    	//console.log(locationItem)
-    });
-    // Operations
-    self.addLocation = function() {
-        self.locations.push(new Location("LUCAS MOYER", 44, -124));
-    }
-    self.removeSeat = function(location) { self.locations.remove(location) }
-    //console.log(self.locationList());
-    //console.log(locations);
-    return self.locationList();
+	//Filters list of displayed locations based on User query string
+	self.filteredList = ko.computed(function(){
+		var filter = self.filter().toLowerCase();
+		if(!filter){
+			return self.locationList();
+		}else{
+			return ko.utils.arrayFilter(self.locationList(), function(item){
+				var match = item.name.toLowerCase().indexOf(filter) >=0;
+				return match;
+			})
+		}
+	},ViewModel);
+
+
+    var stringStartsWith = function (string, startsWith) {          
+	    string = string || "";
+	    if (startsWith.length > string.length)
+	        return false;
+	    return string.substring(0, startsWith.length) === startsWith;
+	};
 }
+
 
 ko.applyBindings(new ViewModel());
