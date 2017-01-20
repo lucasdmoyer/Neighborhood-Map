26119ba63c9cdac87c1c85c419c72ffa7c13f381
Lucas Moyer <Lucas Moyer>
2017-01-19 23:40:38 -0800

    type: feat.
    body: Added basic toggling function for markers
    
    footer: Version 9

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
-var locations =  [
+function initMap(list) {
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 15,
+			center: {lat: 44.636, lng: -124.053}
+		});
+	model.map.push(map);
+
+	console.log(model.locations);
+	for (i=0; i<model.locations.length; i++) {
+		var data = model.locations[i];
+		var latLng = new google.maps.LatLng(data.lat, data.lng);
+		console.log(data.lng);
+
+		var marker = new google.maps.Marker({
+			position: {lat: data.lat, lng: data.lng},
+			map: map,
+			title: data.name,
+			label: data.name
+		});
+		model.gmarkers.push(marker);
+
+
+	}
+
+    // Add a marker clusterer to manage the markers.
+    //var markerCluster = new MarkerClusterer(map, markers,
+        //{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
+var model = {
+	map: [],
+	gmarkers :[],
+	locations: [
         {
             name : 'Starbucks',
             lat : 44.63731107,
@@ -9,30 +41,47 @@ var locations =  [
             lat : 44.63726527,
             lng : -124.05372798
         }
-    ]
+    ],
+    filteredLocations : []
+
+}  
 
 // Class to represent a row in the seat reservations grid
-function Location(name, lat, lng) {
+var Location = function(name, lat, lng) {
     var self = this;
-    this.name = ko.observable(name);  
-    this.lat = ko.observable(lat);
-    this.lng = ko.observable(lng);
+    this.name = name;  
+    this.lat =lat;
+    this.lng =lng;
 }
+
+function toggleMarkers() {
+  for (i = 0; i < model.gmarkers.length; i++) {
+    if (model.gmarkers[i].getMap() != null) model.gmarkers[i].setMap(null);
+    else model.gmarkers[i].setMap(model.map[0]);
+  }
+}
+
 // Overall viewmodel for this screen, along with initial state
-function ViewModel() {
+var ViewModel = function() {
     var self = this;
     //self.locationList = ko.observableArray(locations);
     self.filter = ko.observable("");
+    /*
 	self.locationList = ko.observableArray([
 	    { name: "Bungle", type: "Bear" },
 	    { name: "George", type: "Hippo" },
 	    { name: "Zippy", type: "Unknown" }
 	]);
-
+	*/
+	self.locationList = ko.observableArray([]);
+	for (i=0; i<model.locations.length; i++) {
+		self.locationList().push(new Location(name=model.locations[i].name, lat=model.locations[i].lat, lng=model.locations[i].lng))
+	}
 	//Filters list of displayed locations based on User query string
 	self.filteredList = ko.computed(function(){
 		var filter = self.filter().toLowerCase();
 		if(!filter){
+			toggleMarkers();
 			return self.locationList();
 		}else{
 			return ko.utils.arrayFilter(self.locationList(), function(item){
@@ -41,14 +90,10 @@ function ViewModel() {
 			})
 		}
 	},ViewModel);
-
-
-    var stringStartsWith = function (string, startsWith) {          
-	    string = string || "";
-	    if (startsWith.length > string.length)
-	        return false;
-	    return string.substring(0, startsWith.length) === startsWith;
-	};
+	model.filteredLocations = self.filteredList();
+	
+    
+	
 }
 
 
